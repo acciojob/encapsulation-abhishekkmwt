4e18ec51a966c03e869adb9e64cd456ae9519e4f
abhishekkmwt <kumawat.abhishek167@gmail.com>
2023-03-14 15:24:04 +0530

    Assignment Completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+      RWOnly obj=new RWOnly();
+      String ans=obj.name; //Error as name is private in RWOnly class and cant be accessed outside class.
+      obj.name="abhishek";
+      System.out.println(ans);
+
+      obj.setter("abhi");
+      String ans1=obj.getter();
+
+      System.out.println(ans1);
+
+  }
 }
